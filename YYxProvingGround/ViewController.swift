d0c22cdb26c1bccf78dbc3eeacafa40c28758b6f
002 <shihli.chen@infinox.cn>
2019-03-22 16:38:32 +0800

    22.2 for Date extension

--- YYxProvingGround/ViewController.swift
+++ YYxProvingGround/ViewController.swift
@@ -12,29 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var startStamp = Double()
+//        dateTest()
         
-        var lastTimeString = "2019-03-22 09:40:00"
-        lastTimeString = lastTimeString.replacingOccurrences(of: "T", with: " ")
-        lastTimeString = lastTimeString.replacingOccurrences(of: "Z", with: "")
+
+    }
+    func dateTest() {
+        let lastTimeString = "2019-03-22 09:40:00"
         
-        
-        
-       
-      
         let yy = lastTimeString.convertUTC0ToCurrent(dateFormat: DateFormatter.ixFormat)
-//        let yy = aa.convertCurrentToUTC0(dateFormat: DateFormatter.ixFormat)
-        
-        
-//        let gg = yy.converStampToUTC0String(dateFormat: DateFormatter.ixFormat)
         
         print(yy)
         
         let xx = yy.convertCurrentToUTC0(dateFormat: DateFormatter.ixFormat)
         print(xx)
-
     }
-   
-
 }
 
