50c2747dda39eeba240276853f671514468dc9c4
young lu <yyxdev@gmail.com>
2019-05-23 20:29:19 +0800

    2019 May 23 v6

--- YYxProvingGround/NetworkTestViewController+Extension.swift
+++ YYxProvingGround/NetworkTestViewController+Extension.swift
@@ -13,6 +13,35 @@ extension NetworkTestViewController{
             YYxErrorHandler.printTestLog(witchStep: 1, message: data.convertToUTF8String())
         })
     }
-
+    func examplePostData() {
+        
+        let url = URL(string: "https://my-json-server.typicode.com/YYxDeveloper/FakeAPI/posts")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        // insert json data to the request
+        do {
+            let theData:Data? = try FileManager.default.readJsonFileFromBundle("HttpsTestPostExample").data(using: .utf8)
+             request.httpBody = theData
+        } catch  {
+            print("a")
+        }
+       
+       
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            YYxErrorHandler.printTestLog(witchStep: 33, message: data.convertToUTF8String())
+//            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+//            if let responseJSON = responseJSON as? [String: Any] {
+//                print(responseJSON)
+//            }
+        }
+        
+        task.resume()
+    }
     
 }
--- YYxProvingGround/NetworkTestViewController.swift
+++ YYxProvingGround/NetworkTestViewController.swift
@@ -14,7 +14,8 @@ class NetworkTestViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-       exampleTestURLconnect()
+       examplePostData()
+//        exampleTestURLconnect()
     }
     
 
