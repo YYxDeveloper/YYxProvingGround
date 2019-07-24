09f33cb0795bfc669e8dd802f81211b69a66784d
young lu <yyxdev@gmail.com>
2019-07-24 16:22:05 +0800

    2019 July 22 v7

--- YYxProvingGround/ViewController/LocalNotificationViewController.swift
+++ YYxProvingGround/ViewController/LocalNotificationViewController.swift
@@ -23,32 +23,12 @@ class LocalNotificationViewController: UIViewController {
 
     @IBAction func buttonCreateNotification(_ sender: Any) {
         //creating the notification content
-        let content = UNMutableNotificationContent()
-        content.title = "Title1"
-        content.body = "Body1"
-        content.sound = .default
-        content.userInfo = ["User": "User1"]
-        
-        var trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        var request = UNNotificationRequest(identifier: "NotificationIdentifier", content: content, trigger: trigger)
-        center.add(request, withCompletionHandler: nil)
+       center.sendLocalNotification()
     }
     
     @IBAction func buttonReadNotifications(_ sender: Any) {
         // Find list of Local Notification the app has already set
-        center.getPendingNotificationRequests { (notifications) in
-            print("Total Pending Notifications: \(notifications.count)")
-            for item in notifications {
-                print("\(item.content.userInfo) \n")
-            }
-        }
-        
-        center.getDeliveredNotifications { (notifications) in
-            print("Total Delivered Notifications: \(notifications.count)")
-            for item in notifications {
-                print("\(item) \n")
-            }
-        }
+       center.getNotificationInfo()
     }
 }
 
