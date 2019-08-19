2d539a71523060f7a6b67b45fa2941bc28801405
呂子揚 <yyxdev@gmail.com>
2019-08-19 14:28:33 +0800

    2019 Aug 19 v2

--- YYxProvingGround/YYX /UIKit+Extension/UIColor+Extension.swift
+++ YYxProvingGround/YYX /UIKit+Extension/UIColor+Extension.swift
@@ -12,6 +12,27 @@ extension UIColor{
     static func giveRGBColor(red:CGFloat,green:CGFloat,blue:CGFloat,opcity:Double)->UIColor{
         return UIColor(red: red/RGB_MAX, green: green/RGB_MAX, blue: blue/RGB_MAX, alpha: 0.3)
     }
+    static func giveMeUIColorByHex (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
     func convertColorToImage() -> UIImage? {
         let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
         UIGraphicsBeginImageContext(rect.size)
