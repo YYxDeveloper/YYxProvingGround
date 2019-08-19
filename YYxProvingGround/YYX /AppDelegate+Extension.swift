129df707376fbbcb4a4105de245a574d8d37f453
呂子揚 <yyxdev@gmail.com>
2019-08-19 10:34:15 +0800

    2019 Aug 19 v1

--- YYxProvingGround/YYX /AppDelegate+Extension.swift
+++ YYxProvingGround/YYX /AppDelegate+Extension.swift
@@ -10,10 +10,10 @@ import Foundation
 import UIKit
 extension AppDelegate{
     //加入於didFinishLaunchingWithOptions
-     func embedNavigationController(witchVCStoryBoardID:String?) {
+    func embedNavigationController(witchVCStoryBoardID:String?,witchstoryBoardName:String?) {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let nav1 = UINavigationController()
-        let mainView = UIViewController.giveMeViewControllerFromStoryBoard(storyBoardName: "Main", storyBoardID:witchVCStoryBoardID ?? "ViewController" ) //ViewController = Name of your controller
+        let mainView = UIViewController.giveMeViewControllerFromStoryBoard(storyBoardName: witchstoryBoardName ?? "Main", storyBoardID:witchVCStoryBoardID ?? "ViewController" ) //ViewController = Name of your controller
         nav1.viewControllers = [mainView]
         self.window!.rootViewController = nav1
         self.window?.makeKeyAndVisible()
