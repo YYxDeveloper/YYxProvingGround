901887e31e9f1e95a31cc49e1e5a55873006a981
呂子揚 <yyxdev@gmail.com>
2019-08-21 12:35:06 +0800

    set LoginMainViewController

---
+++ YYxProvingGround/LoginMainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginMainViewController.swift
+//  YYxProvingGround
+//
+//  Created by young.lu on 2019/8/21.
+//  Copyright © 2019 002. All rights reserved.
+//
+
+import UIKit
+
+class LoginMainViewController: YYxScrollViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
