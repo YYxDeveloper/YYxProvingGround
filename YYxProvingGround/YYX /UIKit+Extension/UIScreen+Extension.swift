8ee4899faa5a6c58589135f49fc77c11bca6690d
呂子揚 <yyxdev@gmail.com>
2019-08-21 13:48:40 +0800

    2019 Aug 21 v3

---
+++ YYxProvingGround/YYX /UIKit+Extension/UIScreen+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIScreen+Extension.swift
+//  YYxProvingGround
+//
+//  Created by young.lu on 2019/8/21.
+//  Copyright © 2019 002. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIScreen{
+    var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+}
--- YYxProvingGround/YYxScrollViewController.swift
+++ YYxProvingGround/YYxScrollViewController.swift
@@ -7,7 +7,12 @@
 //
 
 import UIKit
-
+/**
+ Relation files
+ UIScreen+Extension
+ 
+ 
+ */
 class YYxScrollViewController: UIViewController {
 
     let scrollView = UIScrollView()
@@ -20,18 +25,12 @@ class YYxScrollViewController: UIViewController {
         setupViews()
         
         view.backgroundColor = .green
-        
-        let screenSize: CGRect = UIScreen.main.bounds
-        let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
-        print("Screen width = \(screenWidth), screen height = \(screenHeight)")
     }
     override func viewDidLayoutSubviews() {
         scrollViewContentSizeUpdate()
     }
   
     override func viewDidAppear(_ animated: Bool) {
-//        print("ttt :: \(scrollContainerView.frame)")
         scrollViewContentSizeUpdate()
 
     }
@@ -54,43 +53,12 @@ class YYxScrollViewController: UIViewController {
     }
     
     func setupViews(){
-        scrollContainerView.addSubview(label1)
-        label1.centerXAnchor.constraint(equalTo: scrollContainerView.centerXAnchor).isActive = true
-        label1.topAnchor.constraint(equalTo: scrollContainerView.topAnchor).isActive = true
-        label1.widthAnchor.constraint(equalTo: scrollContainerView.widthAnchor, multiplier: 3/4).isActive = true
-        
-        scrollContainerView.addSubview(label2)
-        label2.centerXAnchor.constraint(equalTo: scrollContainerView.centerXAnchor).isActive = true
-        label2.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 25).isActive = true
-        label2.widthAnchor.constraint(equalTo: scrollContainerView.widthAnchor, multiplier: 3/4).isActive = true
-        label2.bottomAnchor.constraint(equalTo: scrollContainerView.bottomAnchor).isActive = true
         
     }
     
-    let label1: UILabel = {
-        let label = UILabel()
-        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut culpa qui officia deserunt mollit anim id est laborum.............."
-        label.numberOfLines = 0
-        label.sizeToFit()
-        label.textColor = UIColor.white
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let label2: UILabel = {
-        let label = UILabel()
-        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        label.numberOfLines = 0
-        label.sizeToFit()
-        label.textColor = UIColor.red
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
     func scrollViewContentSizeUpdate()  {
         let updatedHeight = scrollContainerView.frame.size.height
-        
-        
-        print("xxx :: \(updatedHeight)")
+        print("scrollContainerView :: \(updatedHeight)")
 
     }
     
