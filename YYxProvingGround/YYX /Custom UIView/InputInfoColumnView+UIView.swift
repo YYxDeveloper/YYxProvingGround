45f44daa4508667da4b7fe51a51e6c080be6a99e
呂子揚 <yyxdev@gmail.com>
2019-08-26 11:44:33 +0800

    set userName layout constraint

--- YYxProvingGround/YYX /Custom UIView/InputInfoColumnView+UIView.swift
+++ YYxProvingGround/YYX /Custom UIView/InputInfoColumnView+UIView.swift
@@ -14,18 +14,25 @@ extension UIView{
         case userName,password,verify,recommend
     }
     func becomeColumnView(witchColumntype:columnType){
+        let dividColor:UIColor = UIColor.giveMeUIColorByHex(hex:"6E6E6")
         
         switch witchColumntype {
         case .userName:
             let imageView = UIView()
             self.addSubview(imageView)
             imageView.backgroundColor = .lightGray
+            
             let textField = UITextField()
             self.addSubview(textField)
             textField.backgroundColor = .orange
             
+            let dividView = UIView()
+            self.addSubview(dividView)
+            dividView.backgroundColor = dividColor
+            
             imageView.translatesAutoresizingMaskIntoConstraints = false
             textField.translatesAutoresizingMaskIntoConstraints = false
+            dividView.translatesAutoresizingMaskIntoConstraints = false
             
             NSLayoutConstraint.activate([
                 imageView.sameAsSuperViewTopAnchor(),
@@ -33,12 +40,17 @@ extension UIView{
                 imageView.sameAsSuperViewBottomAnchor(),
                 imageView.sameAsSuperViewHeightAnchor(),
                 imageView.heightEqualWidth(),
-                imageView.trailingKissLeading(withView: textField),
                 
+                dividView.topAnchor.sameAsTopAnchor(withView: dividView.firstSuperView, marginSpace: 10),
+                dividView.bottomAnchor.sameAsBottomAnchor(withView: dividView.firstSuperView, marginSpace: 10),
+                dividView.leadingKissTrailing(withView: imageView),
+                dividView.trailingKissLeading(withView: textField),
+                dividView.widthAnchor.constraint(equalToConstant: 2),
+               
                 textField.sameAsSuperViewTopAnchor(),
                 textField.sameAsSuperViewBottomAnchor(),
-                textField.sameAsSuperViewHeightAnchor(),
-                textField.sameAsSuperViewTrailingAnchor()
+                textField.sameAsSuperViewTrailingAnchor(),
+                textField.heightAnchor.constraint(equalToConstant: 20),
                 
                 
                 ])
--- YYxProvingGround/YYX /UIKit+Extension/UIImage+Extension.swift
+++ YYxProvingGround/YYX /UIKit+Extension/UIImage+Extension.swift
@@ -38,4 +38,16 @@ extension UIImage{
         }
         return nil
     }
+   static func giveMeBackgroundColorAsImage(color: UIColor) -> UIImage {
+        
+    let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+    UIGraphicsBeginImageContext(rect.size)
+    let context = UIGraphicsGetCurrentContext()
+    context!.setFillColor(color.cgColor)
+    context!.fill(rect)
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return img!
+    }
+    
 }
