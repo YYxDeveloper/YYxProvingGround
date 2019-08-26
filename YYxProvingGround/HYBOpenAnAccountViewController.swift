7a6a59f58e27b929e52bb494070b4a5826aea7a1
呂子揚 <yyxdev@gmail.com>
2019-08-26 19:09:42 +0800

    edit HYBOpenAnAccountViewController

---
+++ YYxProvingGround/HYBOpenAnAccountViewController.swift
@@ -0,0 +1,52 @@
+//
+//  HYBOpenAnAccountViewController.swift
+//  YYxProvingGround
+//
+//  Created by young.lu on 2019/8/26.
+//  Copyright © 2019 002. All rights reserved.
+//
+
+import UIKit
+
+class HYBOpenAnAccountViewController: UIViewController {
+     let headBackgroundView = UIImageView()
+     let headLabelsView = UIStackView()
+     let selectedView = UIStackView()
+     let bodyView = UIView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        editHeadView()
+        
+    }
+    func editHeadView() {
+        func constraintHeadBackgroundView(){
+            self.view.addSubview(headBackgroundView)
+            headBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+            headBackgroundView.image = UIImage(named: "bg")
+            
+           NSLayoutConstraint.activate([
+                headBackgroundView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 1),
+                headBackgroundView.sameAsSuperViewLeadingAnchor(),
+                headBackgroundView.sameAsSuperViewTrailingAnchor(),
+            
+            
+           ])
+        }
+        constraintHeadBackgroundView()
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
