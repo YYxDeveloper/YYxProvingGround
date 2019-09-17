b4d23886ff721805a7385adc49ae5719bcea268c
呂子揚 <yyxdev@gmail.com>
2019-09-17 15:19:21 +0800

    2019 Sep 17 v1

--- YYxProvingGround/YYX /Extension+NSPredicate.swift
+++ YYxProvingGround/YYX /Extension+NSPredicate.swift
@@ -19,6 +19,8 @@ extension NSPredicate{
         case EMAIL    = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         case CHINESE  = "^[\\u4E00-\\u9FA5]+$"
         case PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[\\s\\S]{6,18}$"
+        //目前注册输入 qwer12345678901234  并没有被提示   要输入 6~14位数字或字母
+        case EngXInt  = "[0-9|a-z|A-Z]{6,14}"
     }
     func isMatchRule(of expression:expressionContent ,witchContent:String) -> Bool {
         let redicate = NSPredicate(format: "SELF MATCHES %@", expression.rawValue)
