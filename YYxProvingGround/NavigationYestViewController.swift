af7df1db8e2261a6ac84cd5ff877e0bf5941fb04
呂子揚 <yyxdev@gmail.com>
2019-10-21 17:10:49 +0800

    NavegationVC example

---
+++ YYxProvingGround/NavigationYestViewController.swift
@@ -0,0 +1,50 @@
+//
+//  NavigationYestViewController.swift
+//  YYxProvingGround
+//
+//  Created by young.lu on 2019/10/21.
+//  Copyright © 2019 002. All rights reserved.
+//
+
+import UIKit
+
+class NavigationYestViewController: UIViewController {
+
+    @IBOutlet weak var topMarginTestView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension UIView {
+    func findConstraint(layoutAttribute: NSLayoutConstraint.Attribute) -> NSLayoutConstraint? {
+        if let constraints = superview?.constraints {
+            for constraint in constraints where itemMatch(constraint: constraint, layoutAttribute: layoutAttribute) {
+                return constraint
+            }
+        }
+        return nil
+    }
+
+    func itemMatch(constraint: NSLayoutConstraint, layoutAttribute: NSLayoutConstraint.Attribute) -> Bool {
+        if let firstItem = constraint.firstItem as? UIView, let secondItem = constraint.secondItem as? UIView {
+            let firstItemMatch = firstItem == self && constraint.firstAttribute == layoutAttribute
+            let secondItemMatch = secondItem == self && constraint.secondAttribute == layoutAttribute
+            return firstItemMatch || secondItemMatch
+        }
+        return false
+    }
+}
