32a7f8671661703a24efdd9207b2e15efc00e953
young.lu <young.lu@test.com>
2020-03-06 13:10:00 +0800

    //

---
+++ YYxProvingGround/DateSelectItem.swift
@@ -0,0 +1,14 @@
+//
+//  DateSelectItem.swift
+//  BurningNote
+//
+//  Created by young.lu on 2020/3/5.
+//  Copyright © 2020 young.lu. All rights reserved.
+//
+
+import UIKit
+
+class DateSelectItem: UICollectionViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    
+}
